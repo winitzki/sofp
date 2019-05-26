5770db6177ead448fd1571e0ac0620ddfe0e1386
sergei.winitzki <winitzki@gmail.com>
2019-05-26 00:08:51 -0700

    add a script to make all derivation comments green

--- make_sofp_pdf.sh
+++ make_sofp_pdf.sh
@@ -17,9 +17,19 @@ t="$name.tex"
 p="$name.pdf"
 z="$name-src.tar.bz2"
 
-"$lyx" --export pdf2 $s
-mv "$p" "1$p"
-"$lyx" --export latex "$s"
-tar jcvf "$z" "$s" "$t" *.jpg "$0"
-"$pdftk" "1$p" attach_files "$z" output "$p"
-rm "1$p"
+if false; then
+	"$lyx" --export pdf2 $s
+	"$lyx" --export latex "$s"
+	mv "$p" "1$p"
+
+else
+	"$lyx" --export pdflatex "$s"
+# Example of inserted color: \scalebox{0.8}{\color{greenunder}\text{outer-identity}:}\quad &
+	LC_ALL=C sed -e 's|^\(.*\\text{.*}.*:\)\( *\\quad \& \)|{\\color{greenunder}\1}\2|' < "$t" > "1$t"
+	for a in 1 2 3 4; do pdflatex --interaction=batchmode "1$t"; done
+	mv "1$t" "$t"
+	tar jcvf "$z" "$s" "$t" *.jpg "$0"
+	"$pdftk" "1$p" attach_files "$z" output "$p"
+	rm "$t" "1$name".*
+
+fi
