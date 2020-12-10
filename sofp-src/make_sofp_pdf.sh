aff2c7551ed1568cc58516fc810b0c0357ed9a6c
sergei.winitzki <sergei.winitzki@workday.com>
2020-12-10 16:53:57 +0100

    reformat build information

--- sofp-src/make_sofp_pdf.sh
+++ sofp-src/make_sofp_pdf.sh
@@ -92,7 +92,8 @@ function insert_examples_exercises_count { # This is replaced in the root file o
 	local diagrams=`cat "$base"-*.tex | LC_ALL=C fgrep -c '\xymatrix{'`
 	local bdate=`date -R`
 	local osinfo=`uname -rs`
-	LC_ALL=C sed -i.bak -e "s|BUILDDATE|$bdate|g; s|BUILDOPERATINGSYSTEM|$osinfo|g; s,NUMBEROFEXAMPLES,$examples,g; s,NUMBEROFEXERCISES,$exercises,g; s,NUMBEROFDIAGRAMS,$diagrams,g; s,NUMBEROFSTATEMENTS,$stmts,g; s,NUMBEROFCODESNIPPETS,$codesnippets,g;" "$target"
+	local pdftex=`pdflatex --version | fgrep pdfTeX\ 3.14`
+	LC_ALL=C sed -i.bak -e "s|PDFTEXVERSION|$pdftex|g;  s|BUILDDATE|$bdate|g; s|BUILDOPERATINGSYSTEM|$osinfo|g; s,NUMBEROFEXAMPLES,$examples,g; s,NUMBEROFEXERCISES,$exercises,g; s,NUMBEROFDIAGRAMS,$diagrams,g; s,NUMBEROFSTATEMENTS,$stmts,g; s,NUMBEROFCODESNIPPETS,$codesnippets,g;" "$target"
 }
 
 function remove_lulu {
