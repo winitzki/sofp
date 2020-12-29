96f26776a325ce40725604953344960b9ba7aaf4
sergei.winitzki <sergei.winitzki@workday.com>
2020-12-29 14:48:44 +0100

    minor adjustment

--- sofp-src/get_lines_with_displaymath_in_new_paragraph.sh
+++ sofp-src/get_lines_with_displaymath_in_new_paragraph.sh
@@ -1,2 +1,5 @@
-egrep -n  -B1 '^\\(\[|begin{align)' "$1"|egrep  '^[0-9]+-$' | sed -e 's/-$//'
+for a in sofp*tex; do 
+  echo "$a"
+  LC_ALL=C egrep -n  -B1 '^\\(\[|begin{align)' "$a" | LC_ALL=C egrep  '^[0-9]+-$' | sed -e 's/-$//'
 # Output consists of several line numbers.
+done
