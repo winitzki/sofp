938a990f4a9ad173574ec5684db3a2f8c02bbb19
Sergei Winitzki <winitzki@users.noreply.github.com>
2021-11-08 03:02:30 -0800

    Chapter 12, Traversable functors (#42)
    * move some exercises to first section
    
    * add a simple exercise
    
    * wip equivalence of foldmap and foldleft
    
    * wip equivalence of foldleft and foldmap
    
    * wip foldmap and foldleft equivalence
    
    * wip
    
    * minor corrections
    
    * update minor
    
    * minor change
    
    * wip equivalence of foldFn and reduceE
    
    * wip monoidal naturality
    
    * minor correction
    
    * add new files
    
    * fix file name
    
    * towards proofs of relational parametricity
    
    * minor updates
    
    * minor updates
    
    * add sample 100 pages file
    
    * minor fix
    
    * wip example proofs
    
    * add more details
    
    * add pdf and tex files
    
    * update slides
    
    * update slides
    
    * towards proof of equivalence of foldLeft and foldMap
    
    * minor update
    
    * towards proof of foldFn property
    
    * more proofs
    
    * towards proof of the transformation law of foldFn
    
    * minor update
    
    * minor update
    
    * finish proof of foldFn law
    
    * reduce verbosity in appendix
    
    * improve formatting
    
    * towards toList equivalence
    
    * minor corrections
    
    * formatting in chapter 1
    
    * minor update
    
    * minor changes
    
    * minor fixe
    
    * update tex
    
    * minor updates
    
    * revise slides for SBTB 2021
    
    * updates in monad chapters
    
    * minor changes
    
    * update slides
    
    * minor update to slides
    
    * explanation about missing laws of fold
    
    * improve explanations
    
    * towards polynomial constructions for toList
    
    * finish constructions for toList
    
    * equivalence of traverse and sequence
    
    * clarify the special law of foldFn
    
    * minor updates after building pdf
    
    * minor updates, avoid "disjoint domain"
    
    * update pdf
    
    * minor updates
    
    * minor update
    
    * minor update
    
    * minor change
    
    * definition of applicative naturality law and applicative morphisms
    
    * minor update
    
    * adding proof of applicative morphism pure_F
    
    * begin proof of traverse identity law equivalence
    
    * minor update
    
    * insert two diagrams
    
    * minor update
    
    * better formatting of diagram
    
    * minor update
    
    * minor update
    
    * update slides
    
    * mninor update
    
    * minor update
    
    * minor update
    
    * better motivation for identity law
    
    * minor corrections
    
    * minor change
    
    * minor update
    
    * minor correction
    
    * corrections in punctuation
    
    * adding a reference to another book
    
    * minor corrections
    
    * minor fixes
    
    * wip towards traversable composition law
    
    * write out the full code example
    
    * add an open problem about traverse
    
    * minor change
    
    * minor update
    
    * minor change
    
    * wip naturality law of sequence
    
    * corrections from Andreas
    
    * adding exercise to prove equivalence of naturality laws
    
    * minor update
    
    * minor update
    
    * minor changes
    
    * update pdf
    
    Co-authored-by: sergei.winitzki <sergei.winitzki@workday.com>

--- sofp-src/extract_scala_snippets.pl
+++ sofp-src/extract_scala_snippets.pl
@@ -3,15 +3,14 @@ while(<>) {
 	while(<>) { last if (m/^\\begin\{lstlisting\}/); };
 	$c += 1;
 	$filename=sprintf("snippet%05d.scala", $c);
-	printf "Code snippet %05d:\n", $c;
-	print "```scala mdoc\n";
+	printf "// Code snippet %05d:\n", $c;
 	while(<>) {
 		last if (m/^\\end\{lstlisting\}/);
-		s,^scala>,,;
-		s,^ammonite@,,;
-		s,^res[0-9]+,// res0,;
+		s,^(scala>|ammonite\@)\s+,,;
+		s,^res[0-9]+\s*:\s*(.+[^\s])\s*=\s*,  mustBe[$1] ,;
+		s,^([0-9a-zA-Z_]+)\s*:\s*(.+[^\s])\s*=\s*,$1 mustBe[$2] ,;
 		s,//([^/]|/[^/])*,\n,;
 		print;
 	};
-	print "```\n\n"; 
+	print "\n"; 
 };
--- sofp-src/sofp-nameless-functions.pre.md
+++ sofp-src/sofp-nameless-functions.pre.md
@@ -1,3 +1,6 @@
-```scala mdoc
-val n: Int = 10
-```
+implicit class MustBeOp[A](val x: A) extends AnyVal {
+	def mustBe[B >: A](y: B): Unit = assert(x == y)
+}
+
+val n: Int = 10 // Defined in the text.
+
