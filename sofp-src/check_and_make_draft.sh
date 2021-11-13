4abbbc90d4386d7194e8b35dde9166a0d2adcada
sergei.winitzki <sergei.winitzki@workday.com>
2021-11-13 21:31:32 +0100

    change page counts

--- sofp-src/check_and_make_draft.sh
+++ sofp-src/check_and_make_draft.sh
@@ -11,7 +11,7 @@ name="sofp"
 draft_pages=738
 
 # The number of pages in each chapter:
-pagecounts=(3 17 42 34 24 49 43 12 63 54 63 62 45 27 148 5 5 6 3 6 6 3 5 49 20 1 2 1 1 7)
+pagecounts=(3 17 42 34 24 49 43 12 63 54 63 62 46 27 148 5 5 6 3 6 6 3 5 49 21 1 2 1 1 7)
 
 # To create the draft version that contains only proofread chapters:
 # cut out from here, including:
