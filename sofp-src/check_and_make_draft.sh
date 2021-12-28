66801bffc7d719cd08c419baa367cc47c4291003
sergei.winitzki <sergei.winitzki@workday.com>
2021-12-28 23:03:18 +0100

    update tex sources

--- sofp-src/check_and_make_draft.sh
+++ sofp-src/check_and_make_draft.sh
@@ -8,10 +8,10 @@ name="sofp"
 # When changing the title of any chapter, make sure it is correctly updated in `draft_title_*` and in the `chapters` array below.
 
 # Expected total number of pages in the book draft:
-draft_pages=746
+draft_pages=745
 
 # The number of pages in each chapter:
-pagecounts=(3 17 42 34 24 49 43 12 63 54 63 62 52 27 148 5 5 6 3 6 6 3 6 49 21 1 2 1 1 7)
+pagecounts=(3 17 42 34 24 49 43 12 63 54 63 62 54 27 148 5 5 6 3 6 6 3 6 48 21 1 2 1 1 7)
 
 # To create the draft version that contains only proofread chapters:
 # cut out from here, including:
