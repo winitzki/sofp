777c5e83ef7a52a0727ae6c6e9e6998c7dee5997
Sergei Winitzki <winitzki@gmail.com>
2025-07-29 18:41:43 +0200

    wip fixing the chapter3-figure problem

--- sofp-src/scripts/chapter3-figure-make-pdf.sh
+++ sofp-src/scripts/chapter3-figure-make-pdf.sh
@@ -5,6 +5,6 @@
 logfile="../build/chapter3-picture-build.log"
 latex chapter3-picture.tex >& "$logfile"
 dvips chapter3-picture.dvi >& "$logfile"
-ps2pdf -dPDFSETTINGS=/prepress -dEmbedAllFonts=true chapter3-picture.ps >& "$logfile"
+ps2pdf -dALLOWPSTRANSPARENCY -dPDFSETTINGS=/prepress -dEmbedAllFonts=true chapter3-picture.ps >& "$logfile"
 mv chapter3-picture.log ../build/
 rm -f chapter3-picture.{tex,aux,dvi,ps}
